b74d5033c2465d75c66df9d6042164f58c83a449
YRDevml <yr.devml@gmail.com>
2025-09-15 11:22:53 +0300

    fix videos

--- src/api/StudyPlatform/types.ts
+++ src/api/StudyPlatform/types.ts
@@ -1,182 +1,182 @@
 export interface Module {
-  id: number;
-  name: string;
+	id: number
+	name: string
 }
 
 export interface Tag {
-  id: number;
-  name: string;
+	id: number
+	name: string
 }
 
 export interface Course {
-  id: number;
-  tags: Tag[];
-  name: string;
-  description: string;
-  image: string;
-  price: string;
-  discount: number;
-  is_discount: boolean;
-  sell_price: string;
-  mine: boolean;
-  course_progress: number;
-  lessons_amount: number;
+	id: number
+	tags: Tag[]
+	name: string
+	description: string
+	image: string
+	price: string
+	discount: number
+	is_discount: boolean
+	sell_price: string
+	mine: boolean
+	course_progress: number
+	lessons_amount: number
 }
 
 // If you expect an array of courses:
-export type CourseList = Course[];
+export type CourseList = Course[]
 
 export interface Lesson {
-  id: number;
-  name: string;
-  is_passed: boolean;
-  is_free: boolean;
-  is_comming_soon: boolean;
+	id: number
+	name: string
+	is_passed: boolean
+	is_free: boolean
+	is_comming_soon: boolean
 }
 
 export interface ModuleDetail {
-  id: number;
-  name: string;
-  description: string;
-  mine: boolean;
-  module_progress: number;
-  lessons_count: number;
-  lessons_list: Lesson[];
+	id: number
+	name: string
+	description: string
+	mine: boolean
+	module_progress: number
+	lessons_count: number
+	lessons_list: Lesson[]
 }
 
 export interface Tag {
-  id: number;
-  name: string;
+	id: number
+	name: string
 }
 
 export interface CourseDetail {
-  id: number;
-  name: string;
-  short_description: string;
-  description: string;
-  price: string;
-  discount: number;
-  is_discount: boolean;
-  sell_price: string;
-  image: string;
-  tags: Tag[];
-  mine: boolean;
-  course_progress: number;
-  lessons_amount: number;
-  course_goal: string;
-  modules: ModuleDetail[];
+	id: number
+	name: string
+	short_description: string
+	description: string
+	price: string
+	discount: number
+	is_discount: boolean
+	sell_price: string
+	image: string
+	tags: Tag[]
+	mine: boolean
+	course_progress: number
+	lessons_amount: number
+	course_goal: string
+	modules: ModuleDetail[]
 }
 
 export interface Video {
-  id: number;
-  title: string;
-  url: string;
+	id: number
+	title: string
+	url: string
 }
 
 export interface NavLesson {
-  id: number;
-  passed: boolean;
-  name: string;
-  is_accessible: boolean;
+	id: number
+	passed: boolean
+	name: string
+	is_accessible: boolean
 }
 
 export interface NavModule {
-  id: number;
-  name: string;
-  passed: boolean;
-  lessons: NavLesson[];
+	id: number
+	name: string
+	passed: boolean
+	lessons: NavLesson[]
 }
 
 export interface LessonDetail {
-  id: number;
-  name: string;
-  description: string;
-  text_home_task: string;
-  is_free: boolean;
-  quize: number;
-  videos: Video[];
-  nava_data: NavModule[];
-  pdf_task: string;
-  is_comming_soon: boolean;
+	id: number
+	name: string
+	description: string
+	text_home_task: string
+	is_free: boolean
+	quize: number
+	videos: string
+	nava_data: NavModule[]
+	pdf_task: string
+	is_comming_soon: boolean
 }
 
 interface Answer {
-  id: number;
-  text: string;
-  is_correct: boolean;
+	id: number
+	text: string
+	is_correct: boolean
 }
 
 interface Question {
-  id: number;
-  question: string;
-  question_type: 'multiple_choice' | 'text';
-  answers?: Answer[];
+	id: number
+	question: string
+	question_type: 'multiple_choice' | 'text'
+	answers?: Answer[]
 }
 
 interface NaviLesson {
-  id: number;
-  name: string;
-  passed: boolean;
-  is_accessible: boolean;
+	id: number
+	name: string
+	passed: boolean
+	is_accessible: boolean
 }
 
 interface NaviData {
-  id: number;
-  name: string;
-  passed: boolean;
-  lessons: NaviLesson[];
+	id: number
+	name: string
+	passed: boolean
+	lessons: NaviLesson[]
 }
 
 export interface Quiz {
-  id: number;
-  title: string;
-  questions: Question[];
-  nava_data: NaviData[];
+	id: number
+	title: string
+	questions: Question[]
+	nava_data: NaviData[]
 }
 
 export interface QuizAnswerPayload {
-  question: number;
-  selected_answer?: number;
-  text_answer?: string;
+	question: number
+	selected_answer?: number
+	text_answer?: string
 }
 
 export interface QuizSubmissionPayload {
-  quiz: number | string;
-  answers: QuizAnswerPayload[];
+	quiz: number | string
+	answers: QuizAnswerPayload[]
 }
 
 export interface QuizSubmissionResponse {
-  id: number;
-  quiz: number;
-  answers: QuizAnswerPayload[];
-  submitted_at: string;
+	id: number
+	quiz: number
+	answers: QuizAnswerPayload[]
+	submitted_at: string
 }
 
 export interface PaymentResponse {
-  payment: {
-    id: number;
-    order_reference: string;
-    status: 'PENDING' | 'SUCCESS' | 'DECLINED';
-    created_at: string;
-    user: number;
-    course: number;
-  };
-  payment_url: string;
+	payment: {
+		id: number
+		order_reference: string
+		status: 'PENDING' | 'SUCCESS' | 'DECLINED'
+		created_at: string
+		user: number
+		course: number
+	}
+	payment_url: string
 }
 
 export interface PaymentStatusCheckResponse {
-  status: string;
+	status: string
 }
 
 export interface QuizAnswerResult {
-  id: number;
-  text_answer: string | null;
-  mentor_comment: string | null;
-  submission: number;
-  question: number;
-  selected_answer: number | null;
+	id: number
+	text_answer: string | null
+	mentor_comment: string | null
+	submission: number
+	question: number
+	selected_answer: number | null
 }
 
 export interface QuizResult {
-  answers: QuizAnswerResult[];
+	answers: QuizAnswerResult[]
 }
